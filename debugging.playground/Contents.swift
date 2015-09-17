dfa670d4f646fdaf4360bcb546ff9ff1f6701443
ChenningZhang <czhang1306@berkeley.edu>
2015-09-16 21:48:13 -0700

    Complete iOS HW1.

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -15,41 +15,45 @@ class Foo {
     var wordB : String!
     
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]
+        wordB = words[1]
     }
     
 //: [EXPLAIN YOUR ANSWER TO Q1 HERE]
     
-
+//The ? is unnecessary because wordA and wordB already return Optionals. Using the optional chaining operator without chaining a further member lookup will have no effect.
     
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
-        let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+    
+    class func arePalindromes(words: [String]) -> Bool! {
+        let reversedWords = words.map({String($0.characters.reverse())})
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
 //: [EXPLAIN YOUR ANSWER TO Q2 HERE]
     
-    
+    //The reason is that i was declared as a constant using let. In other words, the value of it cannot be changed once declared. Hence the '++' operator cannot be used on i.
+    //Also another problem is that the value of the variable numElements is never changed after being declared and throughout the function. Therefore declaring it as a constant using let is better than using var.
+    //Third problem is that the function initially needs to be called on an instance, add the keyword class makes the function able to be called on a class.
+    //The loop returns true if the word passed in is a palindrome, and false otherwise.
     
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
-        var lenA = wordA.characters.count
-        var lenB = wordB.characters.count
+    class func isAnagram(wordA: String, wordB: String) -> Bool! {
+        var countLetters : [Character : Int] = [Character: Int]()
+        let lenA = wordA.characters.count
+        let lenB = wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,18 +79,18 @@ class Foo {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 }
 
 //: [EXPLAIN YOUR ANSWER TO Q3 HERE]
-
+    //Add "class" keyword to make it a class function, otherwise it cannot be called on a class.
 
 //: **Do not** change anything below.
 //: You should be able to call the methods as is.
@@ -95,6 +99,7 @@ Foo.isAnagram("hello", wordB: "what")
 
 var palindromes = ["hih", "racecar", "mom", "wow"]
 var notPalindromes = ["gene", "shawn", "hello"]
+
 Foo.arePalindromes(palindromes)
 Foo.arePalindromes(notPalindromes)
 
